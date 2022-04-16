ca96feb1ec9cc922d527d116f8f19339f1d0d448
Jarryd Walker <98364864+Jarryd-Walker@users.noreply.github.com>
2022-04-16 18:54:52 +1200

    Added more codewars solutions

--- 8 kyu solutions.js
+++ 8 kyu solutions.js
@@ -602,3 +602,7 @@ function ifChuckSaysSo(a){
 function setAlarm(employed, vacation){
   return employed == true && vacation == false
 }
+// 8 kyu - BASIC: Making Six Toast.
+function sixToast(num) {
+  return num === 6 ? 0 : num > 6 ? num - 6 : 6 - num
+}

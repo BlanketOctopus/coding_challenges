d14384f74f229556caa7fdcaeabb0b9357890e18
Jarryd Walker <98364864+Jarryd-Walker@users.noreply.github.com>
2022-05-10 22:08:51 +1200

    Added more codewars solutions

--- 8 kyu solutions.js
+++ 8 kyu solutions.js
@@ -654,3 +654,13 @@ function makeUpperCase(str) {
 function twiceAsOld(dadYearsOld, sonYearsOld) {
   return dadYearsOld >= sonYearsOld * 2 ? dadYearsOld - sonYearsOld * 2 : sonYearsOld * 2 - dadYearsOld   
 }
+// 8 kyu - Rock Paper Scissors!
+const rps = (p1, p2) => {
+  if(p1 === p2){
+    return 'Draw!'
+  }else if((p1 === 'rock' && p2 === 'scissors') || (p1 === 'scissors' && p2 === 'paper') || (p1 === 'paper' && p2 === 'rock')){
+    return 'Player 1 won!'
+  }else{
+    return 'Player 2 won!'
+  }
+}
